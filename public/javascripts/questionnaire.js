a5ceeede073cd9384085e9a5229e35f4b4a60c77
Roman Simecek <roman@dreammachine.(none)>
2009-06-08 13:08:54 +0200

    fade in/out of the answers in the backend

--- public/javascripts/questionnaire.js
+++ public/javascripts/questionnaire.js
@@ -1,1 +1,12 @@
 var newQuestionCounter = 0;
+
+var newQuestionCounter = 0;
+
+document.observe('dom:loaded', function() {
+  $$('.question').each( function(s){ 
+    var value = $(s).down('p.content').down('select').value;
+    if(value == 2 || value == 3){
+      $(s).down('div').down('a').addClassName('show');
+    }
+  });
+});
